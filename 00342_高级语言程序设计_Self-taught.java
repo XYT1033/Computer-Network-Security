942c918cf31fe0e813995017268d39b0f93c47d3
git_global <git_global@qq.com>
2020-07-18 17:37:14 +0800

    11

--- 00342_高级语言程序设计_Self-taught.java
+++ 00342_高级语言程序设计_Self-taught.java
@@ -92,6 +92,218 @@ D.    0x7&7
 
 
 
+单元综合测试(二)
+
+1. 以下数组的初始化合法的是：
+
+A。   char a[6] ="string";
+D.   int a[]={0,1,2,3,4,5};
+
+
+答： D     。  A中表示字符串"string"的数组应该是char a[7];
+
+
+
+
+
+2. 设 char s[] ="BeijingChina";     ,执行语句 printf("%d,%d\n",strlen(s),sizeof(s));     后输出的结果是：   
+
+答：  12,13   。   本题中strlen()的功能是计算字符数组的字符数，不包含字符串结束标识符'\0'。    
+sizeof用于计算数据类型所占内存空间，用字节数表示(包含'\0'所占的内存空间 )
+
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS   //xyt 
+#include<stdio.h>
+
+void main() // c语言 strlen  sizeof  http://c.biancheng.net/view/342.html    
+{
+	char s[] = "我想"; // strlen 是一个函数，它用来计算指定字符串 str 的长度，但不包括结束字符（即 null 字符）
+	printf("%d,%d", strlen(s), sizeof(s));
+
+	system("pause");//xyt
+}
+
+
+
+
+
+4. 设 char s[20] = "World",   t[20] = "Asia";   , 执行下列语句后的输出结果是： 
+    strcat(t,s);
+	printf("%s,%d\n",t,strlen(t));
+	
+	
+	
+答： AsiaWorld,9  
+
+
+
+#define _CRT_SECURE_NO_WARNINGS   //xyt 
+#include<stdio.h>
+
+int main() // c语言 strcat   http://c.biancheng.net/c/strcat.html 
+{
+	char s[20] = "World", t[20] = " Asia";
+	strcat(t, s);   // strcat() 函数用来将两个字符串连接（拼接）起来。 
+	printf("%s,%d\n", t, strlen(t));
+
+	system("pause");//xyt
+}
+
+
+
+  
+
+6. 定义二维数组并初始化，正确的是：
+
+A. int a[3][] ={{1,2,3},{4,5,6}};
+B. int a[][3] ={{1,2,3},{4,5,6}};
+
+答： B .        二维数组的列长度不可省略， 若初始化时给所有元素赋初值或分行给所有行的前若干个元素赋初值时，
+行长度可以省略。 
+
+
+
+
+7.   设 char str1[]="ABCD";
+     char str2[]={'A','B','C','D'};
+
+   则：  数组str1的长度大于 str2 的长度 
+
+
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS   //xyt 
+#include<stdio.h>
+
+int main() // 
+{
+	char str1[] = "ABCD";
+	char str2[] = { 'A','B','C','D' };
+	printf("%d,%d\n", strlen(str1), strlen(str2));
+	printf("%d,%d\n", sizeof(str1), sizeof(str2));
+
+	system("pause");//xyt
+}
+
+
+
+
+
+
+8  . 一个源文件中定义的全局变量的作用域为:
+A.  本文件的全部范围  
+D.  从定义该变量开始至本文件结束 
+
+
+
+答： D 。      全局变量的作用域是从定义点到整个程序结束，即从定义该变量时开始到该程序结束，全局变量都产生作用。
+
+
+
+
+
+
+11. 设 float x,y,z;     , 函数调用语句 fun(z,((x,y),x+2));  中参数的个数是：  
+
+B。 3 
+C。 2 
+
+
+
+答： C          函数中的参数是以逗号分隔的。本题中函数调用语句fun(z,((x,y),x+z));  中参数分别为z 和 ((x,y),x+z) , 故有2个参数。
+
+
+
+
+
+13. 函数中定义的变量类型可以是：
+A. auto型
+B. static 型 
+C.  register 型 
+D.  以上三种类型 
+
+答： D  
+
+
+
+
+
+
+
+16. C 语言中数组元素的下标从( 0 ) 开始，下标不能越界。 
+
+17. 字符串是由若干字符组成的，其最后一个字符是：    '\0' 
+
+18 . 若有以下定义： double w[10],则w数组元素下标的上限为：  9 
+
+
+19, 设short a[5][3];   如果数组a 首地址为1000，则 a[1][0]的地址为：    1006 
+
+
+
+20. 使用系统函数对两个字符串进行比较，该系统函数的名字是：  strcmp  
+
+
+
+21.  函数中的变量，若不专门声明为 static 型，则隐含为 (  自动型 )变量 
+
+
+22. 值传递是把实参的 (值) 传给被调用函数的形参。 
+
+
+23. auto型 变量存储在内存的( 动态存储区 )
+
+24。  C程序中调用字符处理库函数 isalpha(),  则需要使用 #include 命令包含头文件：   ctype.h  
+
+
+25.   若函数要去返回一个值，则应在函数体中有一条 ( return 返回值表达式 ) 语句。 
+
+
+
+
+
+程序分析题:  将输出结果写出来
+26.   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- 00342_高级语言程序设计_Self-taught_code.java
+++ 00342_高级语言程序设计_Self-taught_code.java
@@ -331,6 +331,468 @@ void main()
 
 
 
+单元综合测试(二)
+
+
+程序分析题:  将输出结果写出来
+
+//*  *  *   *  *  *  *  *  *   *  *  *  *  *  *   *  *  *  
+26.   
+
+
+#define _CRT_SECURE_NO_WARNINGS   //xyt 
+#include<stdio.h>
+
+int main()
+{
+	int i, j;
+	int a[3][3], b[3][3], x;
+	for(i=0;i<3;i++)
+	for (j = 0;j < 3;j++)
+	{
+		scanf("%d,", &x); //输入 1,2,3,4,5,6,7,8,9 回车
+		a[i][j] = x;
+	}
+
+	for (i = 0;i < 3;i++)
+	for (j = 0;j < 3;j++)
+	b[j][i] = a[j][i];
+	for (i = 0;i < 3;i++)
+	{
+		printf("\n");
+		for(j=0;j<3;j++)
+		printf("%d",b[i][j]);
+	}
+
+
+
+
+
+	system("pause");//xyt
+}
+
+
+
+
+
+
+答案： 
+123
+456
+789
+
+
+
+
+
+
+
+
+
+
+
+
+//*  *  *   *  *  *  *  *  *   *  *  *  *  *  *   *  *  *  
+27 
+
+
+#define _CRT_SECURE_NO_WARNINGS   //xyt 
+#include<stdio.h>
+
+int main()
+{
+	int a[3][4] = {1,3,5,7,12,14,16,18,9,6,4,2},j,k,m,n,x;
+	x = m = n = 0;
+	for(j=2;j>-1;j--)
+	for(k=0;k<4;k++)
+		if (x < a[j][k])
+		{
+			x = a[j][k];
+			m = j;
+			n = k;
+		}
+	
+	printf("a[%d][%d]=%d\n",m,n,x);
+
+
+
+
+	system("pause");//xyt
+}
+
+
+
+
+
+
+答：  a[1][3]=18
+
+
+
+
+
+
+
+
+
+
+
+//*  *  *   *  *  *  *  *  *   *  *  *  *  *  *   *  *  *  
+28. 
+#include<stdio.h>
+int i = 1;
+int fun(int j)
+{
+	return(++i+j);
+}
+
+
+int main()
+{
+	int j;
+	for (j = 1;j <= 3;j++)
+		printf("%d\n",fun(j));
+	
+	system("pause");//xyt
+}
+
+
+
+答：
+3
+5
+7
+
+
+
+
+
+
+
+
+//*  *  *   *  *  *  *  *  *   *  *  *  *  *  *   *  *  *  
+29.
+#include<stdio.h>
+#define PR printf("x=%d,y=%d\n",x,y)
+int x = 2, y = 8;   //全局变量
+
+void fun()
+{
+	x++;
+	PR;
+}
+
+int main()
+{
+	int x = 8;
+	fun();
+	PR;
+	
+	system("pause");//xyt
+}
+
+
+
+
+答： 
+x=3,y=8
+x=8,y=8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+程序填充题 
+
+//*  *  *   *  *  *  *  *  *   *  *  *  *  *  *   *  *  *  
+30.   从键盘为二维整型数组t1输入数据，输出该二维数组的转置t2.
+
+
+#define _CRT_SECURE_NO_WARNINGS   //xyt 
+#include<stdio.h>
+#define M 2
+#define N 3
+
+void main()
+{
+	int t1[M][N], t2[N][M], i, j;
+	printf("二维数组t1:\n");
+	for (i = 0;i < M;i++)
+	{
+		for (j = 0;j < N;j++)
+		{
+			scanf("%d",&t1[i][j]);   //
+			printf("%5d",t1[i][j]);
+			t2[j][i] = t1[i][j];     //
+		}
+		printf("\n");
+	}
+	printf("二维数组t2:\n");
+	for (i = 0;i <N ;i++)         //
+	{
+		for (j = 0;j < M;j++)
+			printf("%5d",t2[i][j]);
+		printf("\n");
+	}
+
+
+	system("pause");//xyt
+}
+
+
+
+
+
+ 上面的不好，总是要键盘输入，beneath 是我 reform 的 
+
+
+
+ #define _CRT_SECURE_NO_WARNINGS   //xyt 
+#include<stdio.h>
+#define M 2
+#define N 3
+
+void main()
+{
+	int t1[M][N] = { {1,2,3},{10,20,30} }, t2[N][M], i, j;
+
+	for (i = 0;i < M;i++)
+	{
+		for (j = 0;j < N;j++)
+		{
+			printf("%5d",t1[i][j]);
+			t2[j][i] = t1[i][j];     //
+		}
+		printf("\n");
+	}
+	printf("二维数组t2:\n");
+	for (i = 0;i <N ;i++)         //
+	{
+		for (j = 0;j < M;j++)
+			printf("%5d",t2[i][j]);
+		printf("\n");
+	}
+
+
+	system("pause");//xyt
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//*  *  *   *  *  *  *  *  *   *  *  *  *  *  *   *  *  *  
+31. 将一字符串中的小写英文字母改成大写  
+
+#define _CRT_SECURE_NO_WARNINGS   //xyt 
+#include<stdio.h>
+
+
+int  main()
+{
+	int i = 0;char str[80] = "abcdef";
+	//scanf("%s",str);
+	while (str[i]!='\0') {  //
+		if (str[i]>='a'  && str[i]<='z')  //
+			str[i] -= 'a' - 'A';
+		i++;//
+	}
+	printf("%s\n",str);
+
+	system("pause");//xyt
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//*  *  *   *  *  *  *  *  *   *  *  *  *  *  *   *  *  *  
+32. 从键盘输入整数n(1<=n<10),分别计算并输出1!,2!,......,n! 
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS   //xyt 
+#include<stdio.h>
+
+
+long int fac(int m)
+{
+	static int c = 1;
+	c *= m;
+	return(c);
+}
+
+void   main()
+{
+	int i, n;
+	scanf("%d",&n);//
+	if (n >= 1 && n < 10)
+		for (i = 1;i <= n;i++)
+			printf("%d! =%1d\n", i,fac(i) );//
+	else
+		printf("input error! \n");
+
+	system("pause");//xyt
+}
+
+
+
+
+
+
+
+
+
+
+程序设计题
+
+33. 从键盘向一维数组中输入10个实数，计算并输出其最大值和平均值 
+//*  *  *   *  *  *  *  *  *   *  *  *  *  *  *   *  *  *  
+
+
+
+#define _CRT_SECURE_NO_WARNINGS   //xyt 
+#include<stdio.h>
+
+
+
+int   main()
+{
+	int i, m;
+	float a[10], total = 0.0, ave, max;
+	for (m = 0;m < 10;m++)
+	scanf("%f", &a[m]);//输入
+	printf("\n");
+	max = a[0];
+
+	for (i = 0;i < 10;i++)
+	{
+		total += a[i];//求和
+		if (a[i] > max)max = a[i];//求最大值
+	}
+	ave = total; // 10;//求平均值
+	printf("max=%f,ave=%f\n",max,ave);
+
+	system("pause");//xyt
+}
+
+
+
+
+ 上面的总要键盘输入，beneath 我 reform 
+
+
+
+#define _CRT_SECURE_NO_WARNINGS   //xyt 
+#include<stdio.h>
+
+
+
+int   main()
+{
+	int i, m;
+	float a[10] = {23,54,22,88,2,43,56,6,345,44}, total = 0.0, ave, max;
+
+	max = a[0];
+
+	for (i = 0;i < 10;i++)
+	{
+		total += a[i];//求和
+		if (a[i] > max)max = a[i];//求最大值
+	}
+	ave = total; // 10;//求平均值
+	printf("max=%f,ave=%f\n", max, ave);
+
+	system("pause");//xyt
+}
+
+
+
+
+
+
+
+//*  *  *   *  *  *  *  *  *   *  *  *  *  *  *   *  *  *  
+34. 
+(1)编写一个计算梯形面积的函数。函数头是：float area(float s,float x,float h),其中变量s、x和h 分别
+表示梯形的上底、下底和高。
+(2)编写主函数从键盘输入梯形的上底、下底和高的值，调用上述函数输出梯形面积(保留小数点后两位)
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS   //xyt 
+#include<stdio.h>
+
+float area(float s, float x, float h)
+{
+	return (s + x) * h * 0.5;
+}
+
+int   main()
+{
+	
+	float s1, x1, h1;
+	scanf("%f%f%f",&s1,&x1,&h1);
+	printf("area=%7.2f\n",area(s1,x1,h1));
+
+	system("pause");//xyt
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
